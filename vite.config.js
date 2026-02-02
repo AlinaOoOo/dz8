3e5300ea1dcb283bb3e05456807f8f965a8a7172
mark3425 <markiila20068@gmail.com>
2026-02-02 20:29:24 +0300

    First commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    outDir: 'dist', // Убедитесь что есть
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  base: './' // ← ВАЖНО: для GitHub Pages
 })
